20312ef3d1806d53ff9a5e386d56d2bb091ab151
Tobias Brandt <Tobias.Brandt@gmail.com>
2024-09-21 22:35:16 +0200

    Adds strategy list endpoint

--- api/src/astranova_api/main.py
+++ api/src/astranova_api/main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
+import json
+
+with open('/app/data/strategy_list.json') as f:
+    data = json.load(f)
+strategies = data['results']
+names = list(el['name'] for el in strategies)
 
 app = FastAPI()
 
-
-@app.get("/api")
-async def root():
-    return {"message": "Hello World"}
-
 @app.get("/api/hello")
-async def root():
+async def hello():
     return {"message": "Hello Api"}
+
+@app.get("/api/list")
+async def list_():
+    return names

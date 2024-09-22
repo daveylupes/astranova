6f46872e2a0d2612cdf7e283dc51c2194e89e209
DaveyLupes <lupscyber@gmail.com>
2024-09-22 15:41:04 +0200

    fix navbar

--- ui/src/components/Navbar.js
+++ ui/src/components/Navbar.js
@@ -1,23 +1,29 @@
-// src/components/Navbar.js
-
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
+import Logo from '../assets/AstraNova_logo.jpg'; // Adjusted import path
+import './../App.css';
 
-const Navbar = () => {
+function Navbar() {
   return (
     <AppBar position="static">
-      <Toolbar>
-        <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>
-          <Typography variant="h6">
-            <img src="path/to/your/logo.png" alt="Astro Nova Logo" style={{ height: '40px', marginRight: '10px' }} />
-            Astro Nova
-          </Typography>
-        </Link>
-        {/* Other navbar items can go here */}
+      <Toolbar className="navbar">
+      <Button className="navbar-button" component={Link} to="/">
+        <img src={Logo} alt="Astro Nova Logo" className="navbar-logo" />
+      </Button>
+        <Typography variant="h6" component="div" className="navbar-title">
+          
+        </Typography>
+        <Button className="navbar-button" component={Link} to="/">
+          Home
+        </Button>
+        <Button className="navbar-button" component={Link} to="/about">
+          About
+        </Button>
+        {/* Add more buttons for additional routes as needed */}
       </Toolbar>
     </AppBar>
   );
-};
+}
 
 export default Navbar;

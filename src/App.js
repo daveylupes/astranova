dec82b67341b71f982bf5a3f479830b9bd6e024b
DaveyLupes <lupscyber@gmail.com>
2024-09-22 09:49:22 +0200

    footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+// src/App.js
+
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Container } from '@mui/material';
+import { Container, Box } from '@mui/material';
 import Footer from './components/Footer';
-
-// Import components
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
@@ -11,15 +11,17 @@ import About from './components/pages/About';
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Container>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          {/* Add more routes as needed */}
-        </Routes>
-      </Container>
-      <Footer /> {/* Added Footer component here */}
+      <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+        <Navbar />
+        <Container sx={{ flexGrow: 1 }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            {/* Add more routes as needed */}
+          </Routes>
+        </Container>
+        <Footer />
+      </Box>
     </Router>
   );
 }
